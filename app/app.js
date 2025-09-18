dbb4c8de6b1270f42d48f0f6b75db5dec0ca9be7
Ubuntu <ubuntu@ip-172-31-5-121.ap-south-1.compute.internal>
2025-09-18 12:28:38 +0000

    sdsddcs

--- app/app.js
+++ app/app.js
@@ -1,3 +1,13 @@
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(Server running at http://0.0.0.0:${PORT});
+const http = require("http");
+
+const PORT = process.env.PORT || 80; // use 3000 if no env var is set
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, world!\n");
 });
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
+});
+
