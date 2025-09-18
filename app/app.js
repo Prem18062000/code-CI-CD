a451369f7beb9d4c671ad97f9f8f7d41380001c4
Ubuntu <ubuntu@ip-172-31-5-121.ap-south-1.compute.internal>
2025-09-18 12:37:26 +0000

    completed

--- app/app.js
+++ app/app.js
@@ -1,10 +1,35 @@
 const http = require("http");
 
-const PORT = process.env.PORT || 80; // use 3000 if no env var is set
+const PORT = 80; // run directly on port 80
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("hi my name is prem IamfromTamilnadu\n");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>My Node App</title>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            background: #f4f4f9;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+          }
+          h1 {
+            font-size: 60px;
+            color: #333;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Hi My name is Prem,I am fromm Tamil Nadu</h1>
+      </body>
+    </html>
+  `);
 });
 
 server.listen(PORT, "0.0.0.0", () => {
